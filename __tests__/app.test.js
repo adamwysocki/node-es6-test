f3a09a9ead5e81502231876954a3befb0798211a
adamwysocki <adam@adamwysocki.me>
2018-01-03 06:45:07 -0500

    added custom expect extension with loDash isMatch

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,39 @@
 const request = require("supertest");
 const app = require("../index.js");
+const diff = require("jest-diff");
+const _ = require("lodash");
 
 describe("index route", () => {
+  expect.extend({
+    toLookLike(received, expected) {
+      received = JSON.parse(received);
+      const pass = _.isEqual(received, expected);
+
+      const message = pass
+        ? () =>
+            `${this.utils.matcherHint(".not.toLookLike")}
+            \n\n
+            Expected value to not be (using Object.is):\n
+              ${this.utils.printExpected(expected)}\n
+            Received:\n
+              ${this.utils.printReceived(received)}`
+        : () => {
+            const diffString = diff(expected, received, {
+              expand: this.expand
+            });
+            return `${this.utils.matcherHint(".toLookLike")} 
+              \n\n
+              Expected value to be (using Object.is):
+                ${this.utils.printExpected(expected)}
+                Received:
+                ${this.utils.printReceived(received)}
+              ${diffString} ? \n\nDifference:\n\n${diffString}}`;
+          };
+
+      return { actual: received, message, pass };
+    }
+  });
+
   afterAll(() => {
     app.server.close();
   });
@@ -14,6 +46,7 @@ describe("index route", () => {
       .expect(response => {
         expect(response.text).toMatch(/hello/);
         expect(response.text).toMatch(/world/);
+        expect(response.text).toLookLike({ hello: "world" });
       });
   });
 
@@ -27,6 +60,7 @@ describe("index route", () => {
         expect(response.text).toMatch(/adam/);
         expect(response.text).toMatch(/skillz/);
         expect(response.text).toMatch(/onpoint/);
+        expect(response.text).toLookLike({ hello: "world", name: "adam", skillz: "onpoint" });
       });
   });
 });
